abc6ef314e767468c1c1c3b31aa3a60ff366ed56
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-10 00:42:23 +0530

    Loops 1.java

---
+++ Loops 1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+        in.close();
+    }
+}
